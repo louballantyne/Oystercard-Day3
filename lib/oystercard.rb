015e6a7b25cee738a629f34be92f4fa69c7c924c
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-03-08 17:01:36 +0000

    Completed tests and code for challenge 9
    Co-Authored-By: Tarandeep Nandhra <76442987+taran314@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,36 @@
 class Oystercard
-  
-  attr_reader :balance
+  DEFAULT_MAXIMUM = 90
+  DEFAULT_MINIMUM = 1
 
-  def initialize(balance = 0)
+  attr_reader :balance, :default_maximum, :journey
+
+  def initialize(balance = 0, default_maximum = DEFAULT_MAXIMUM)
     @balance = balance
+    @default_maximum = default_maximum
+    @journey = false
   end
 
   def top_up(money)
+    fail "Top-up exceeds the predetermined maximum" if @balance + money > @default_maximum
     @balance += money
+  end
+
+  def deduct(money)
+    fail "The deducted amount exceeds the total remaining balance" if @balance - money < 0
+    @balance -= money
+  end
+
+  def touch_in
+    fail "Not enough money to touch in" if @balance < DEFAULT_MINIMUM
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
+  end
+
+  private def in_journey?
+    @journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  test_card = Oystercard.new(20)
   it "has a balance of zero" do
     expect(subject.balance).to eq(0)
   end
@@ -8,6 +9,36 @@ describe Oystercard do
   describe 'top_up' do
     it "will add money to the balance of the card" do
       expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
+    it "will return an error if the total would exceed the default maximum" do
+      expect { subject.top_up(1 + subject.default_maximum) }.to raise_error("Top-up exceeds the predetermined maximum")
+    end
+  end
+
+  describe 'deduct' do
+    it 'deducts a fare from the balance of the card' do
+      expect { test_card.deduct(15) }.to change { test_card.balance }.by -15
+    end
+    it "will return an error if the deducted amount exceeds the total remaining" do
+      expect { test_card.deduct(10) }.to raise_error("The deducted amount exceeds the total remaining balance") 
+    end
+  end
+
+  describe 'touch_in' do
+    it 'touches in a card and sets journey to true' do
+      expect { test_card.touch_in }.to change { test_card.journey }.to be true
+    end
+
+    it 'will return an error if the card has less than £1 balance' do
+      expect { subject.touch_in }.to raise_error("Not enough money to touch in")
+    end
+  end
+
+  describe 'touch_out' do
+    it "touches out a card and sets the journey to false" do
+      # How can we isolate this unit test?
+      test_card.touch_in
+      expect { test_card.touch_out }.to change { test_card.journey }.to be false
     end
   end
 end
@@ -23,3 +54,15 @@ end
 # In order to protect my money from theft or loss
 # As a customer
 # I want a maximum limit (of £90) on my card
+
+# In order to pay for my journey
+# As a customer
+# I need my fare deducted from my card
+
+# In order to get through the barriers.
+# As a customer
+# I need to touch in and out.
+
+# In order to pay for my journey
+# As a customer
+# I need to have the minimum amount (£1) for a single journey.
